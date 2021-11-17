2520182c5908b31e98efe73e773aac9a1f1fa442
fintoci <fintoci@student.ethz.ch>
2021-11-17 13:30:09 +0000

    Add routing.

--- controllers/controller.py
+++ controllers/controller.py
@@ -22,30 +22,53 @@ class Controller(object):
         """Resets switches state"""
         [controller.reset_state() for controller in self.controllers.values()]
 
-    def get_port_num_of_host(self, switch_name)
+    def get_port_and_mac_of_host(self, switch_name):
         host_name = switch_name + "_h0"
-        return self.topo.node_to_node_port_num(switch_name, host_name)
+        host_port = self.topo.node_to_node_port_num(switch_name, host_name)
+        host_mac = self.topo.node_to_node_mac(host_name, switch_name)
+        return str(host_port), host_mac
+
+    def configure_host_port(self, cont, p4switch):
+        host_port, host_mac = self.get_port_and_mac_of_host(p4switch)
+        print(host_port, host_mac)
+        cont.pvs_add("MyParser.host_port", host_port)
+        cont.table_add(
+            "host_port_to_mac", action_name="reconstruct_packet", 
+            match_keys=[host_port], action_params=[host_mac])
+
+    # Delete this when we have proper routing tabels.
+    def setup_test_tables_from_FRA_to_MUC(self):
+        self.controllers["FRA"].table_add(
+            "ipv4_forward", action_name="set_nhop", 
+            match_keys=["10.0.6.1"], action_params=["7"])
+        self.controllers["FRA"].table_add(
+            "ipv4_forward", action_name="set_nhop", 
+            match_keys=["10.0.13.1"], action_params=["4"])
+        print("Thirftprot FRA::", self.topo.get_thrift_port("FRA"))
+        self.controllers["MUN"].table_add(
+            "ipv4_forward", action_name="set_nhop", 
+            match_keys=["10.0.6.1"], action_params=["3"])
+        self.controllers["MUN"].table_add(
+            "ipv4_forward", action_name="set_nhop", 
+            match_keys=["10.0.13.1"], action_params=["5"])
 
     def connect_to_switches(self):
         """Connects to switches"""
         
         for p4switch in self.topo.get_p4switches():
             thrift_port = self.topo.get_thrift_port(p4switch)
-            cont = SimpleSwitchThriftAPI(thrift_port)
-            
-            # Configure the host_port
-            host_port = get_port_num_of_host(p4switch)
-            cont.pvs_add("MyParser.host_port", str(host_port))
-
-            self.controllers[p4switch] = cont
-
-
+            self.controllers[p4switch] = SimpleSwitchThriftAPI(thrift_port)
+        
     def run(self):
         """Run function"""
+        # Setup tables and varsets.
+        for p4switch in self.topo.get_p4switches():
+            self.configure_host_port(self.controllers[p4switch], p4switch)
 
     def main(self):
         """Main function"""
         self.run()
+
 
 
 def get_args():
--- p4src/include/headers.p4
+++ p4src/include/headers.p4
@@ -21,7 +21,7 @@ struct metadata {
 
 }
 
-
+typedef bit<9>  egressSpec_t;
 typedef bit<48> macAddr_t;
 typedef bit<32> ip4Addr_t;
 
