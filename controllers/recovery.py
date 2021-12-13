79a8c41f2b17334c13aa6d1b7a6ec000b6405ad6
wfloris <wfloris@ethz.ch>
2021-12-13 16:49:09 +0000

    Use more efficient encoding for generated routing info

--- controllers/recovery.py
+++ controllers/recovery.py
@@ -28,6 +28,48 @@ def read_settings():
 SETTINGS = read_settings()
 
 
+vertex_names = ["MAN", "MAN_h0", 
+                "GLO", "GLO_h0",
+                "BRI", "BRI_h0", 
+                "LON", "LON_h0",
+                "AMS", "AMS_h0", 
+                "EIN", "EIN_h0",
+                "BER", "BER_h0", 
+                "FRA", "FRA_h0", 
+                "MUN", "MUN_h0",
+                "LIL", "LIL_h0", 
+                "PAR", "PAR_h0", 
+                "REN", "REN_h0",
+                "BAR", "BAR_h0", 
+                "MAD", "MAD_h0", 
+                "POR", "POR_h0", 
+                "LIS", "LIS_h0" ]
+
+def encode_edge(e):
+    return int(vertex_names.index(e[0])) << 5 + int(vertex_names.index(e[1]))
+
+def decode_edge(e: int):
+    edge = int(e)
+    if edge == -1:
+        return ''
+    f = edge >> 5
+    t = edge & (31)
+    return (vertex_names[f], vertex_names[t])
+
+def encode_vertex(v: str):
+    try: 
+        return int(vertex_names.index(v))
+    except:
+        return -1
+
+def decode_vertex(v: int):
+    vertex = int(v) # Oh lord, do I hate python.
+    if vertex == -1:
+        return ''
+    return vertex_names[vertex]
+
+
+
 class Fast_Recovery_Manager(object):
     @staticmethod
     def add_delay_weight(g: Graph):
@@ -110,9 +152,9 @@ class Fast_Recovery_Manager(object):
         failure_rlfas = {}
         with open(config_file, 'r') as f:
             for entry in json.load(f)["map"]:
-                failures = frozenset(entry["failures"])
-                failure_rts[failures] = entry["routing_tbl"]
-                failure_rlfas[failures] = entry["Rlfas"]
+                failures = frozenset([decode_edge(f) for f in entry["f"]])
+                failure_rts[failures] = entry["t"]
+                failure_rlfas[failures] = entry["R"]
         return failure_rts, failure_rlfas
 
 
@@ -438,6 +480,65 @@ class Fast_Recovery_Manager(object):
                             "routing_tbl": routing_tbl,\
                             "Rlfas": Rlfas}
         return scenario
+        
+    @staticmethod
+    def __form_routing_enc(graph, switches, hosts, failures=None):
+        """
+            Forms the routing state for the current failure scenario
+            Returns a scenario data structure
+        """
+        
+        #dijkstra handles removing the failed links here
+        distances, shortest_paths = Fast_Recovery_Manager.dijkstra(graph, failures)
+        nexthops = Fast_Recovery_Manager.compute_nexthops(shortest_paths, switches, hosts, failures)
+        lfas = Fast_Recovery_Manager.compute_lfas(graph, switches, hosts, distances, nexthops, failures)
+        sim_cost_paths = Fast_Recovery_Manager.compute_scmps(lfas, distances, SETTINGS["scmp_threshold"])
+        Rlfas = Fast_Recovery_Manager.compute_Rlfas(graph, switches, nexthops, lfas, failures)
+        
+        routing_tbl = {}
+        for sw in switches:
+            routing_tbl[encode_vertex(sw)] = {}
+            for host, this_nexthops in nexthops[sw]:
+                try:
+                    lfa = [encode_vertex(lfas[sw][host][0])]
+                    scmp = [encode_vertex(x) for x in sim_cost_paths[sw][host]]
+                except:
+                    #no lfa
+                    lfa = []
+                    scmp = []
+                routing_tbl[encode_vertex(sw)][encode_vertex(host)] = {"n":[encode_vertex(v) for v in this_nexthops], "l":lfa, "s": scmp}
+
+        Rlfas_enc = {encode_vertex(k): {encode_vertex(e_k): encode_vertex(e_v) for e_k, e_v in e.items()} for k, e in Rlfas.items()}
+        scenario = {"f": [encode_edge(x) for x in failures],\
+                            "t": routing_tbl,\
+                            "R": Rlfas_enc}
+        return scenario
+
+    @staticmethod
+    def decode_routing_table(rt_enc):
+        rt = {}
+        try:
+            for switch, m in rt_enc.items():
+                sw = decode_vertex(switch)
+                rt[sw] = {}
+                for h, entry in m.items():
+                    host = decode_vertex(h)
+                    this_nexthops = [decode_vertex(v) for v in entry["n"]]
+                    if entry["l"]:
+                        lfa = decode_vertex(entry['l'][0])
+                    else:
+                        lfa = ''
+                    scmps = [decode_vertex(s) for s in entry["s"]]
+                    rt[sw][host] = {"nexthops":this_nexthops, "lfa":lfa, "scmps": scmps}
+        except KeyError as err:
+            print("An Error occurred while decoding the routing table: ", err)
+        return rt
+
+
+    @staticmethod
+    def decode_rlfas(rlfas_enc):
+        return {decode_vertex(k): {decode_vertex(e_k): decode_vertex(e_v) for e_k, e_v in e.items()} for k, e in rlfas_enc.items()}
+
 
     @staticmethod
     def precompute_routing(graph: Graph, switches: List[str], hosts, all_failures: List[List[Tuple[str, str]]] = None):
@@ -452,7 +553,7 @@ class Fast_Recovery_Manager(object):
             if not all_failures:
                 all_failures = [None]
             for failures in all_failures:
-                scenario = Fast_Recovery_Manager.__form_routing(graph, switches, hosts, failures)
+                scenario = Fast_Recovery_Manager.__form_routing_enc(graph, switches, hosts, failures)
                 scenarios.append(scenario)
             map["map"] = scenarios
             json.dump(map, f)
@@ -466,13 +567,13 @@ class Fast_Recovery_Manager(object):
     def query_routing_state(self, failures=[]):
         """Called by controller to retrieve routing state given failures"""
         try:
-            rt = self.failure_rts[frozenset(failures)]
-            rlfa = self.failures_rlfas[frozenset(failures)]
+            rt = self.decode_routing_table(self.failure_rts[frozenset(failures)])
+            rlfa = self.decode_rlfas(self.failures_rlfas[frozenset(failures)])
             print(f"Recovery: loaded routing tables and rlfas from config for failures {failures}")
             return rt, rlfa
         except KeyError:
             scenario = Fast_Recovery_Manager.__form_routing(self.topo, self.switches, self.hosts, failures)
-            print(f"Scenario not found in config. Recomputing...")
+            print(f"Scenario for {failures} not found in config. Recomputing...")
             print("[*] Scenario:\n")
             print(scenario)
             return scenario["routing_tbl"], scenario["Rlfas"]
